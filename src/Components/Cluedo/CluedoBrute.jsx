f1a190102159283c2dc5d87c75d93a2a91fbcfc1
Bo <bo.robbrecht@student.howest.be>
2021-12-02 15:19:38 +0100

    Able to load & sort clues into different states

--- src/Components/Cluedo/CluedoBrute.jsx
+++ src/Components/Cluedo/CluedoBrute.jsx
@@ -4,6 +4,10 @@ import {call} from "../../Helpers/api";
 
 const CluedoBrute = () => {
     const [operations, setOperations] = useState([])
+    const [rooms, setRooms] = useState([]);
+    const [weapons, setWeapons] = useState([]);
+    const [suspects, setSuspects] = useState([]);
+
     const bruteForce = () => {
         const gameId = call(process.env.REACT_APP_URL_NEW)
             .then(response => {
@@ -12,18 +16,37 @@ const CluedoBrute = () => {
             })
             .catch(response => {
                 setOperations(["Something went wrong ¯\\_(ツ)_/¯"])
-                return null;
+                return undefined;
             })
+        if (gameId) {
+            try {
+                call(process.env.REACT_APP_URL_CLUES)
+                    .then(response => {
+                        setRooms(response.filter(item => item.type === "room"));
+                        setWeapons(response.filter(item => item.type === "weapon"));
+                        setSuspects(response.filter(item => item.type === "suspect"));
+                        setOperations(operations.push("Got all clues loaded & sorted"));
+                    })
+                    .then(() => {
+
+                    })
+
+
+            } catch (e) {
+                setOperations(["Something went wrong ¯\\_(ツ)_/¯"])
+            }
+
+        }
     }
 
     return (
         <>
-        <Button onClick={bruteForce} value={"Too hard?"}/>
-        <div>
-            <ul>
-                {operations.map(op => <li>{op}</li>)}
-            </ul>
-        </div>
+            <Button onClick={bruteForce} value={"Too hard?"}/>
+            <div>
+                <ul>
+                    {operations.map(op => <li>{op}</li>)}
+                </ul>
+            </div>
         </>
     )
 }
