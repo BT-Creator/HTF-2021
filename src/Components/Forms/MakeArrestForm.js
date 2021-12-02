9776b38fb5d57b567ec7b76d9b76993b5ef93906
Bo <bo.robbrecht@student.howest.be>
2021-12-02 13:49:36 +0100

    Added arrestation form & redirect to end game page

--- src/Components/Forms/MakeArrestForm.js
+++ src/Components/Forms/MakeArrestForm.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { useSettings } from "../context/useSettings";
+import {BaseForm} from "./BaseForm";
+import {call} from "../../Helpers/api";
 
 /*
    HET ARRESTATIE FORMULIER
@@ -10,10 +12,19 @@ import { useSettings } from "../context/useSettings";
 export const MakeArrestForm = ({ gameKey, onArrest }) => {
   const { settings } = useSettings();
 
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    call(`${process.env.REACT_APP_URL_ACCUSE}?key=${gameKey}`, 'POST', {
+        room: evt.target[0].value,
+        weapon: evt.target[1].value,
+        suspect: evt.target[2].value
+    })
+        .then(onArrest)
+  }
   return (
     <div>
       <h2>Maak een arrestatie</h2>
-      <p>Maak een formulier om een arrestatie te maken.</p>
+      <BaseForm onSubmitForm={handleSubmit}/>
     </div>
   );
 };
